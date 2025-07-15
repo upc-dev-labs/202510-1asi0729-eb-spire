85cdda11fcac998b2d88cdfd4533cb47cfd74d65
Salim Ramirez <salimramirezm@gmail.com>
2025-07-14 22:33:55 -0500

    feat: enable auditing

--- src/main/java/com/spire/platform/u20201e843/PracticeEbSpireApplication.java
+++ src/main/java/com/spire/platform/u20201e843/PracticeEbSpireApplication.java
@@ -2,7 +2,9 @@ package com.spire.platform.u20201e843;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PracticeEbSpireApplication {
 
