7bede07cd2a78fd7bdf8f23778641736377ce349
Salim Ramirez <salimramirezm@gmail.com>
2025-07-14 22:21:47 -0500

    chore: initial commit

---
+++ src/main/java/com/spire/platform/u20201e843/PracticeEbSpireApplication.java
@@ -0,0 +1,13 @@
+package com.spire.platform.u20201e843;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeEbSpireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeEbSpireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spire/platform/u20201e843/PracticeEbSpireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spire.platform.u20201e843;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeEbSpireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
