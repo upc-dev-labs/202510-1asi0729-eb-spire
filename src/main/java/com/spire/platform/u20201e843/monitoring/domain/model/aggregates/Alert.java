a2542da00ec41913d0e6bea66901834c03ccf6d0
Salim Ramirez <salimramirezm@gmail.com>
2025-07-15 04:25:20 -0500

    feat(monitoring): add AttributeOverride in SatelliteCode of Alert aggregate

--- src/main/java/com/spire/platform/u20201e843/monitoring/domain/model/aggregates/Alert.java
+++ src/main/java/com/spire/platform/u20201e843/monitoring/domain/model/aggregates/Alert.java
@@ -4,6 +4,7 @@ import com.spire.platform.u20201e843.monitoring.domain.model.commands.CreateAler
 import com.spire.platform.u20201e843.monitoring.domain.model.valueobjects.AlertType;
 import com.spire.platform.u20201e843.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 import com.spire.platform.u20201e843.shared.domain.model.valueobjects.SatelliteCode;
+import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
@@ -17,7 +18,10 @@ import java.time.LocalDateTime;
 public class Alert extends AuditableAbstractAggregateRoot<Alert> {
 
     @Embedded
-    @Column(nullable = false)
+    @AttributeOverride(
+            name = "satelliteCode",
+            column = @Column(name = "satellite_code", nullable = false)
+    )
     private SatelliteCode satelliteCode;
 
     @NotNull
