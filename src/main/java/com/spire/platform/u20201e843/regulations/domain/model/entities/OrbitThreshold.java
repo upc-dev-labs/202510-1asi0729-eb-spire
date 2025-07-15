ee9ac34cb1a76c2b4252584f107cf84356b24748
Salim Ramirez <salimramirezm@gmail.com>
2025-07-15 02:28:59 -0500

    feat(regulations): add OrbitThreshold

---
+++ src/main/java/com/spire/platform/u20201e843/regulations/domain/model/entities/OrbitThreshold.java
@@ -0,0 +1,34 @@
+package com.spire.platform.u20201e843.regulations.domain.model.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@With
+public class OrbitThreshold {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String orbitClass;
+
+    @NotNull
+    @Min(1)
+    private Integer maxSafeDuration;
+
+    public OrbitThreshold(String orbitClass,Integer maxSafeDuration) {
+        this.orbitClass = orbitClass;
+        this.maxSafeDuration = maxSafeDuration;
+    }
+}
