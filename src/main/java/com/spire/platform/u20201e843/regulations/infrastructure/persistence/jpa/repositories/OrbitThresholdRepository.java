f760741893a1b610c457af86e14ad01727e7c0c8
Salim Ramirez <salimramirezm@gmail.com>
2025-07-15 02:53:47 -0500

    feat(regulations): update OrbitThresholdRepository

--- src/main/java/com/spire/platform/u20201e843/regulations/infrastructure/persistence/jpa/repositories/OrbitThresholdRepository.java
+++ src/main/java/com/spire/platform/u20201e843/regulations/infrastructure/persistence/jpa/repositories/OrbitThresholdRepository.java
@@ -4,7 +4,12 @@ import com.spire.platform.u20201e843.regulations.domain.model.entities.OrbitThre
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrbitThresholdRepository extends JpaRepository<OrbitThreshold,Long> {
 
+    Optional<OrbitThreshold> findByOrbitClass(String orbitClass);
+
+    boolean existsByOrbitClass(String orbitClass);
 }
