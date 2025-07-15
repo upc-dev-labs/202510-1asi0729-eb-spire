3308ef8d3147fa166dc1076ab4861eaa6779dd37
Salim Ramirez <salimramirezm@gmail.com>
2025-07-15 00:21:14 -0500

    feat(monitoring): add Alert aggregate

---
+++ src/main/java/com/spire/platform/u20201e843/monitoring/domain/model/aggregates/Alert.java
@@ -0,0 +1,38 @@
+package com.spire.platform.u20201e843.monitoring.domain.model.aggregates;
+
+import com.spire.platform.u20201e843.monitoring.domain.model.commands.CreateAlertCommand;
+import com.spire.platform.u20201e843.monitoring.domain.model.valueobjects.AlertType;
+import com.spire.platform.u20201e843.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import com.spire.platform.u20201e843.shared.domain.model.valueobjects.SatelliteCode;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+public class Alert extends AuditableAbstractAggregateRoot<Alert> {
+
+    @Embedded
+    @Column(nullable = false)
+    private SatelliteCode satelliteCode;
+
+    @NotNull
+    @Column(nullable = false)
+    private AlertType alertType;
+
+    @NotNull
+    @Column(nullable = false)
+    private LocalDateTime registeredAt;
+
+    protected Alert() {}
+
+    public Alert(CreateAlertCommand command) {
+        this.satelliteCode = new SatelliteCode(command.satelliteCode());
+        this.alertType = AlertType.fromLabel(command.alertType());
+        this.registeredAt = LocalDateTime.now();
+    }
+}
