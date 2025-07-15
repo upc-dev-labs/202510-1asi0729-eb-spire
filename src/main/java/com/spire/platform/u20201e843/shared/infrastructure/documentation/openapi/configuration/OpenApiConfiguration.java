6a32bf1c801e438ae3f8d07155c25a78bcbf5501
Salim Ramirez <salimramirezm@gmail.com>
2025-07-14 22:33:24 -0500

    feat(shared): add documentation for open api

---
+++ src/main/java/com/spire/platform/u20201e843/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -0,0 +1,43 @@
+package com.spire.platform.u20201e843.shared.infrastructure.documentation.openapi.configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    // Properties
+    @Value("${spring.application.name}")
+    String applicationName;
+
+    @Value("${documentation.application.description}")
+    String applicationDescription;
+
+    @Value("${documentation.application.version}")
+    String applicationVersion;
+
+    // Methods
+
+    @Bean
+    public OpenAPI spirePlatformOpenApi() {
+        // General configuration
+        var openApi = new OpenAPI();
+        openApi
+                .info(new Info()
+                        .title(this.applicationName)
+                        .description(this.applicationDescription)
+                        .version(this.applicationVersion)
+                        .license(new License().name("Apache 2.0")
+                                .url("https://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Spire Global Platform wiki Documentation")
+                        .url("https://spire.com/our-data/terms-of-use"));
+
+        // Return the OpenAPI configuration object with all the settings
+        return openApi;
+    }
+}
